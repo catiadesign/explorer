1d02223ab965f3f1c97af19f1b3952542b627dcf
CATIA design <50929680+catiadesign@users.noreply.github.com>
2021-01-10 09:35:27 +0100

    Update download.php

--- download.php
+++ download.php
@@ -1,5 +1,19 @@
 <?php
 
+    define('PATH', $_SERVER["DOCUMENT_ROOT"] . $_GET['path']);
+    define('FILE_NAME', basename($_GET['path']));
+    define('SIZE_LIMIT', 190000000); //190MB
+    define('LOG_DOWNLOADS', true);
+    define('LOG_FILE', 'logs.log');
+
+    header("Expires: 0");
+    header("Cache-Control: no-cache");
+    header("Pragma: no-cache");
+    header("Content-Transfer-Encoding: binary");
+
+    ignore_user_abort(true);
+    ob_implicit_flush();
+
     function folderSize($dir) {
         $size = 0;
         foreach ($dir as $file) {
@@ -22,43 +36,28 @@
         return $zip;
     }
 
-    header("Expires: 0");
-    header("Cache-Control: no-cache");
-    header("Pragma: no-cache");
-    header("Content-Transfer-Encoding: binary");
-
-    ignore_user_abort(true);
-    ob_implicit_flush();
-
-    $path = $_SERVER["DOCUMENT_ROOT"] . $_GET['path'];
-    $downloadSizeLimit = 190000000; //190MB
-    $files_array = glob($path . '/*.*');
-    $name = basename($_GET['path']);
-
-    define('LOG_DOWNLOADS', true);
-    define('LOG_FILE', 'logs.log');
-
     //Files Download
-    if (strpos($path, '.') !== false) {
-        if (file_exists($path)) {
+    if (strpos(PATH, '.') !== false) {
+        if (file_exists(PATH)) {
             header("Content-Type: application/octet-stream");
-            header("Content-Disposition: attachment; filename=" . $name);
-            header("Content-Length:" . filesize($path));
-            readfile($path);
+            header("Content-Disposition: attachment; filename=" . FILE_NAME);
+            header("Content-Length:" . filesize(PATH));
+            readfile(PATH);
         }
     //Folder Files Download and Compress
     } else {
         //if Folder has Files or SIZE smaller than 190MB
-        if (folderSize($files_array) > 0 && folderSize($files_array) < $downloadSizeLimit) {
-            CreteArchive($files_array, $name);
+        $files_array = glob(PATH . '/*.*');
+        if (folderSize($files_array) > 0 && folderSize($files_array) < SIZE_LIMIT) {
+            CreteArchive($files_array, FILE_NAME);
             if (connection_aborted()) {
-                unlink($name);
+                unlink(FILE_NAME);
             }
             header("Content-Type: application/zip");
-            header("Content-Disposition: attachment; filename=" . $name . ".zip");
-            header("Content-Length:" . filesize($name));
-            readfile($name);
-            unlink($name);
+            header("Content-Disposition: attachment; filename=" . FILE_NAME . ".zip");
+            header("Content-Length:" . filesize(FILE_NAME));
+            readfile(FILE_NAME);
+            unlink(FILE_NAME);
         //if Folder has NO Files
         } else {
             header("Content-Type: text/plain");
@@ -75,7 +74,7 @@
         $ip = $_SERVER['REMOTE_ADDR'];
         //$info = file_get_contents("http://ipinfo.io/{$ip}");
         //. "   Full info: " . $info
-        @fputs($f, "Date: " . date("m.d.Y g:ia"). "   IP: " . $ip . "   File: ".$name. "   Path: " . $_GET['path'] . "\n");
+        @fputs($f, "Date: " . date("m.d.Y g:ia"). "   IP: " . $ip . "   File: ". FILE_NAME . "   Path: " . $_GET['path'] . "\n");
         @fclose($f);
     }
     
