615d4a407d70f86cf5ae0c503c22eb0660ecb728
CATIA design <50929680+catiadesign@users.noreply.github.com>
2021-01-22 09:55:20 +0100

    Update download.php

--- download.php
+++ download.php
@@ -4,6 +4,7 @@
     define('FILE_NAME', basename($_GET['path']));
     define('SIZE_LIMIT', 190000000); //190MB
     define('LOG_DOWNLOADS', true);
+    define('EXT_CHECK', false);
     define('LOG_FILE', 'logs.log');
 
     header("Expires: 0");
@@ -13,6 +14,38 @@
 
     ignore_user_abort(true);
     ob_implicit_flush();
+
+    $allowed_ext = array (
+      // archives
+      'zip' => 'application/zip',
+    
+      // documents
+      'pdf' => 'application/pdf',
+      'doc' => 'application/msword',
+      'xls' => 'application/vnd.ms-excel',
+      'ppt' => 'application/vnd.ms-powerpoint',
+      
+      // executables
+      'exe' => 'application/octet-stream',
+    
+      // images
+      'gif' => 'image/gif',
+      'png' => 'image/png',
+      'jpg' => 'image/jpeg',
+      'jpeg' => 'image/jpeg',
+    
+      // audio
+      'mp3' => 'audio/mpeg',
+      'wav' => 'audio/x-wav',
+    
+      // video
+      'mpeg' => 'video/mpeg',
+      'mpg' => 'video/mpeg',
+      'mpe' => 'video/mpeg',
+      'mov' => 'video/quicktime',
+      'avi' => 'video/x-msvideo'
+    );
+
 
     function folderSize($dir) {
         $size = 0;
@@ -36,36 +69,50 @@
         return $zip;
     }
 
+    //file extension
+    $fext = strtolower(substr(strrchr(FILE_NAME, "."), 1));
+    //
+    $files_array = glob(PATH . '/*.*');
+
     //Files Download
-    if (strpos(PATH, '.') !== false) {
-        if (file_exists(PATH)) {
+    if (strpos(PATH, '.') && file_exists(PATH)) {
+        if (EXT_CHECK) {
+            if (array_key_exists($fext, $allowed_ext)) {
+                header("Content-Type: application/octet-stream");
+                header("Content-Disposition: attachment; filename=" . FILE_NAME);
+                header("Content-Length:" . filesize(PATH));
+                readfile(PATH);
+            } else {
+                header("Content-Type: text/plain");
+                header("Content-Disposition: attachment; filename=ReadMe.txt");
+                readfile("ReadMe.txt");          
+            }
+        } else {
             header("Content-Type: application/octet-stream");
             header("Content-Disposition: attachment; filename=" . FILE_NAME);
             header("Content-Length:" . filesize(PATH));
-            readfile(PATH);
-        }
-    //Folder Files Download and Compress
-    } else {
-        //if Folder has Files or SIZE smaller than 190MB
-        $files_array = glob(PATH . '/*.*');
-        if (folderSize($files_array) > 0 && folderSize($files_array) < SIZE_LIMIT) {
-            $newName = FILE_NAME . rand();
-            CreteArchive($files_array, $newName);
-            header("Content-Type: application/zip");
-            header("Content-Disposition: attachment; filename=" . $newName . ".zip");
-            header("Content-Length:" . filesize($newName));
-            readfile($newName);
-            if (connection_aborted()) {unlink($newName);}
-            else {unlink($newName);}
-        //if Folder has NO Files
-        } else {
-            header("Content-Type: text/plain");
-            header("Content-Disposition: attachment; filename=ReadMe.txt");
-            readfile("ReadMe.txt");
+            readfile(PATH);            
         }
     }
+    //Compress if Folder has Files or SIZE smaller than 190MB
+    else if (folderSize($files_array) > 0 && folderSize($files_array) < SIZE_LIMIT) {
+        $newName = FILE_NAME . rand();
+        CreteArchive($files_array, $newName);
+        header("Content-Type: application/zip");
+        header("Content-Disposition: attachment; filename=" . $newName . ".zip");
+        header("Content-Length:" . filesize($newName));
+        readfile($newName);
+        if (connection_aborted()) {unlink($newName);}
+        else {unlink($newName);}
+    }
+    //Info Message
+    else {
+        header("Content-Type: text/plain");
+        header("Content-Disposition: attachment; filename=ReadMe.txt");
+        readfile("ReadMe.txt");
+    }
     
-    // log downloads
+    //log downloads
     if (!LOG_DOWNLOADS) die();
     $f = @fopen(LOG_FILE, 'a+');
     if ($f) {
