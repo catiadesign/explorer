976df54e7a67bf5536476c19fa28d6e230efc187
CATIA design <50929680+catiadesign@users.noreply.github.com>
2021-02-01 22:35:59 +0100

    Update user_register.php

--- user_register.php
+++ user_register.php
@@ -18,6 +18,7 @@
     $email = mysqli_real_escape_string($con, $_POST['txt_email']);
     $password = mysqli_real_escape_string($con, $_POST['txt_pwd']);
     
+    $invalidusername = ["root", "su", "x"];
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
     $hash = password_hash($password, PASSWORD_DEFAULT);
 
@@ -25,7 +26,7 @@
     $result = mysqli_query($con, $sql_query);
     $row = mysqli_fetch_array($result);
 
-    if ($row['email'] != $email && preg_match($email_exp, $email) && $uname != "" && $fullname != "" && $email != "" && $password != "") {
+    if ($row['email'] != $email && preg_match($email_exp, $email) && !in_array($uname, $invalidusername) && $uname != "" && $fullname != "" && $email != "" && $password != "") {
         $sql = "INSERT INTO users (username, fullname, email, password) VALUES ('" . $uname . "', '" . $fullname. "', '" . $email . "' , '" . $hash . "')";
         if (mysqli_query($con, $sql)) {
             $_SESSION['uname'] = $email;
@@ -37,7 +38,7 @@
             echo 'Check for empty fields!<br />';
         }
         
-        if (in_array($uname, ["root", "su", "x"])) {
+        if (in_array($uname, $invalidusername)) {
             echo 'Username not allowed!<br />';
         }
         
