acb8d409244b304a272d201e30ca44d73f6b0c83
CATIA design <50929680+catiadesign@users.noreply.github.com>
2021-02-02 14:53:55 +0100

    Update user_login.php

--- user_login.php
+++ user_login.php
@@ -14,15 +14,11 @@
     $verify = password_verify($password, $row['password']);
 
     $error_message = '';
-    if ($row['email'] != $email || empty($email)) {
-        $error_message .= 'Email not valid!<br />';
-    }
-    if (!$verify || empty($password)) {
-        $error_message .= 'Password not valid!<br />';
-    }
-    if ($row['email'] == $email && $verify && $row['active'] != 1) {
-        $error_message .= 'User not activated!<br />';
-    }
+    if ($row['email'] != $email || empty($email)) {$error_message .= 'Email not valid!<br />';}
+    
+    if (!$verify || empty($password)) {$error_message .= 'Password not valid!<br />';}
+    
+    if ($row['email'] == $email && $verify && $row['active'] != 1) {$error_message .= 'User not activated!<br />';}
     
     if (strlen($error_message) > 0) {
         echo $error_message;
